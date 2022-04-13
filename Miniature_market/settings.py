5aa6c7d2e93203569f7125662cada3e3d2f1ef0c
Yavor Lozev <yavor.lozev@gmail.com>
2022-04-13 22:36:54 +0300

    removed unnecessary env variable

--- Miniature_market/settings.py
+++ Miniature_market/settings.py
@@ -57,7 +57,7 @@ WSGI_APPLICATION = 'Miniature_market.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE'),
+        'ENGINE': "django.db.backends.postgresql",
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
