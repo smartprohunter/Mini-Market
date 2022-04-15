490ce1f10ee8f2c1a5c43a6d44a75567b2e936b0
Yavor Lozev <yavor.lozev@gmail.com>
2022-04-15 13:11:00 +0300

    added new migrations

--- Miniature_market/settings.py
+++ Miniature_market/settings.py
@@ -8,7 +8,6 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'sk')
 DEBUG = os.getenv.__get__('DEBUG', bool)
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(' ')
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
